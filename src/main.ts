f81ac491ead8ae93710f44443502b824d42eb6b8
Anton Moilanen <anton.a.moilanen@gmail.com>
2023-04-01 18:15:30 +0400

    Add Vuetify, remove some auto-generated code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import App from "./App.vue";
+import "./assets/main.css";
 
-import './assets/main.css'
+const vuetify = createVuetify({
+    components,
+    directives,
+  });
 
-createApp(App).mount('#app')
+createApp(App).use(vuetify).mount("#app");
