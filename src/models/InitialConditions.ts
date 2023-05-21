09f6a6a97790b1d79d82d5189827a98f16091e68
Anton Moilanen <anton.a.moilanen@gmail.com>
2023-05-21 15:20:32 +0400

    Prepare for providing more readable phase inputs in the form of "pi something"

--- src/models/InitialConditions.ts
+++ src/models/InitialConditions.ts
@@ -24,3 +24,31 @@ export class InitialConditions {
     return JSON.stringify(this, null, 2);
   }
 }
+
+export class RawFrequencyAndPhase {
+  frequency: string;
+  phase: string;
+  constructor(frequency: string, phase: string) {
+    this.frequency = frequency;
+    this.phase = phase;
+  }
+  //TODO: Implement and add tests
+  parse(): FrequencyAndPhase {
+    return new FrequencyAndPhase(parseFloat(this.frequency), parseFloat(this.phase))
+  }
+}
+
+export class RawInitialConditions {
+  x: RawFrequencyAndPhase;
+  y: RawFrequencyAndPhase;
+  constructor(x: RawFrequencyAndPhase, y: RawFrequencyAndPhase) {
+    this.x = x;
+    this.y = y;
+  }
+  parse(): InitialConditions {
+    return new InitialConditions(this.x.parse(), this.y.parse());
+  };
+  public toString(): string {
+    return JSON.stringify(this, null, 2);
+  }
+}
