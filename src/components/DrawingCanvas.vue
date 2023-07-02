18352211c459661e1f272edb7d5b19c702b9ea85
Anton Moilanen <anton.a.moilanen@gmail.com>
2023-07-03 01:35:11 +0400

    Extract the DrawingCanvas component, use the Composition API to draw the component

---
+++ src/components/DrawingCanvas.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+import { ref, onMounted } from 'vue'
+
+const props = defineProps({
+  width: {
+    type: Number,
+    required: true
+  },
+  height: {
+    type: Number,
+    required: true
+  }
+})
+
+const state = reactive({
+  ctx: null as (null | CanvasRenderingContext2D)
+})
+
+const root = ref(null)
+
+onMounted(() => {
+  const $el = root.value!
+  const canvas = $el as HTMLCanvasElement;
+  state.ctx = canvas.getContext('2d');
+})
+
+async function clear() {
+  state.ctx?.clearRect(0, 0, props.width, props.height)
+}
+
+async function drawPoint(x: number, y: number) {
+  let canvasX = x + props.width / 2
+  let canvasY = y + props.height / 2
+  state.ctx?.fillRect(canvasX, canvasY, 1, 1);
+}
+
+defineExpose({
+  clear,
+  drawPoint
+})
+</script>
+
+<template>
+  <canvas ref="root" :width="width" :height="height"></canvas>
+</template>
+
+<style scoped>
+  canvas {
+    border: 1px dotted;
+  }
+</style>
--- src/components/Vizualization.vue
+++ src/components/Vizualization.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { findCommonPeriod } from '@/math/CommonPeriodFinder';
 import { InitialConditions } from '@/models/InitialConditions';
+import DrawingCanvas from '@/components/DrawingCanvas.vue';
 
 defineProps({
   initialConditions: {
@@ -19,14 +20,13 @@ defineProps({
 </script>
 
 <script lang="ts">
-
 function sleep(time: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
 // Pessimisticly large value to draw the whole curve
 const DEFAULT_MAX_TIME_UNITS = 100
-const TIME_TICKS_IN_TIME_UNIT = 5000
+const TIME_TICKS_IN_TIME_UNIT = 10000
 
 export default {
   methods: {
@@ -34,36 +34,25 @@ export default {
       let commonPeriod = findCommonPeriod(this.initialConditions.x.frequency, this.initialConditions.y.frequency)
       return commonPeriod || DEFAULT_MAX_TIME_UNITS
     },
-    getDrawingContext(): CanvasRenderingContext2D | null {
-      const canvas = document.getElementById('oscillator-plane') as HTMLCanvasElement;
-      return canvas.getContext('2d');
-    },
-    drawPointOnCanvas(ctx: CanvasRenderingContext2D, x: number, y: number) {
-      let canvasX = x + this.width / 2
-      let canvasY = y + this.height / 2
-      ctx.fillRect(canvasX, canvasY, 1, 1);
-    },
     async iterateThroughTime(f: (currentTime: number) => Promise<void>) {
       let maxTimeunits = this.findMaxTimeUnits()
       let maxTime = maxTimeunits * TIME_TICKS_IN_TIME_UNIT
       for (let currentTimeUnit = 0; currentTimeUnit < maxTime; currentTimeUnit++) {
-        const currentTime = currentTimeUnit / TIME_TICKS_IN_TIME_UNIT
+        const currentTime = currentTimeUnit * TIME_TICKS_IN_TIME_UNIT
         await f(currentTime)
       }
     },
     async render() {
-      const ctx = this.getDrawingContext()
+      const canvas = this.$refs.canvasRef as typeof DrawingCanvas
       const amplitude = this.width / 2
 
-      if (ctx) {
-        ctx.clearRect(0, 0, this.width, this.height)
-        this.iterateThroughTime(async currentTime => {
-           let x = amplitude * Math.cos(this.initialConditions.x.frequency * currentTime + this.initialConditions.x.phase)
-           let y = amplitude * Math.cos(this.initialConditions.y.frequency * currentTime + this.initialConditions.y.phase)
-           this.drawPointOnCanvas(ctx, x, y)
-           //await sleep(0.001)
-         })
-      }
+      await canvas.clear()
+      this.iterateThroughTime(async currentTime => {
+        let x = amplitude * Math.cos(this.initialConditions.x.frequency * currentTime + this.initialConditions.x.phase)
+        let y = amplitude * Math.cos(this.initialConditions.y.frequency * currentTime + this.initialConditions.y.phase)
+        await canvas.drawPoint(x, y)
+        //await sleep(0.001)
+      })
     }
   },
   watch: {
@@ -77,13 +66,7 @@ export default {
 <template>
   <v-container class="canvas">
     <v-row cols="12">
-      <canvas id="oscillator-plane" :width="width" :height="height"></canvas>
+      <DrawingCanvas ref="canvasRef" :width="width" :height="height"></DrawingCanvas>
     </v-row>
   </v-container>
 </template>
-
-<style scoped>
-  #oscillator-plane {
-    border: 1px dotted;
-  }
-</style>
