cfcb077d8ea9b2171a0a976f2f72cc956f65e650
Anton Moilanen <anton.a.moilanen@gmail.com>
2023-09-09 23:21:02 +0400

    Add pinia as a dependency

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
-import { createApp } from "vue";
-import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
-import VueMathjax from 'vue-mathjax-next';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
-import App from "./App.vue";
-import "./assets/main.css";
+import { createApp } from "vue"
+import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import VueMathjax from 'vue-mathjax-next'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import App from "./App.vue"
+import "./assets/main.css"
 
+const pinia = createPinia()
 const vuetify = createVuetify({
     components,
     directives,
     theme: {
       defaultTheme: 'light'
     }
-  });
+  })
 
 createApp(App)
   .use(VueMathjax)
   .use(vuetify)
-  .mount("#app");
+  .use(pinia)
+  .mount("#app")
