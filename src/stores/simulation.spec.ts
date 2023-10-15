57bcd87a744d6a96a7bdecac10c1aff4307a441d
Anton Moilanen <anton.a.moilanen@gmail.com>
2023-10-16 00:50:44 +0400

    Add test for startDrawing

--- src/stores/simulation.spec.ts
+++ src/stores/simulation.spec.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it, beforeEach } from 'vitest'
 import { setActivePinia, createPinia } from 'pinia'
+import { InitialConditions, InitialConditionsInput, FrequencyAndPhaseInput, FrequencyAndPhase } from '@/models/InitialConditions'
 
 import { useSimulationStore, DrawingState } from '@/stores/simulation'
 
@@ -9,8 +10,26 @@ describe("simulation store", () => {
     setActivePinia(createPinia())
   })
 
-  it("should have default state 'Initial'", () => {
+  it("should have default state 'Initial', no active visualization id", () => {
     const store = useSimulationStore()
     expect(store.drawingState).toBe(DrawingState.Initial)
+    expect(store.activeVisualization).to.be.null
+  })
+
+  it("should update conditions and record active visualization id on startDrawing", () => {
+    const store = useSimulationStore()
+    store.$patch({
+      conditionsInput:  new InitialConditionsInput(
+        new FrequencyAndPhaseInput("2", "4"),
+        new FrequencyAndPhaseInput("3", "5")
+      )
+    })
+    store.startDrawing()
+    expect(store.activeVisualization).to.not.be.null
+    expect(store.conditions).toEqual(new InitialConditions(
+      new FrequencyAndPhase(2, 4),
+      new FrequencyAndPhase(3, 5)
+    ))
+    expect(store.drawingState).toBe(DrawingState.Started)
   })
 })
